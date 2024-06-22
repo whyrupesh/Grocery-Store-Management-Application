9ce1bee67316e1385edd3ad3ba2648ca8541d2ea
whyrupesh <srupeshkumar800@gmail.com>
2024-06-22 23:40:38 +0530

    data saved to db successfully from product page

--- server/src/models/Products.js
+++ server/src/models/Products.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
 const ProductSchema = new mongoose.Schema({
-  productname: { type: String, required: true, unique: true },
+  productname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   quantity: { type: String, required: true },
   price: { type: String, required: true },
-  //   savedRecipes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
 });
 
 export const ProductModel = mongoose.model("products", ProductSchema);
